88d46aafe7950c1fde8f4cd4115f519294c853bf
Tezkerek <andrei.ancuta@gmail.com>
2022-01-16 20:30:04 +0200

    refactor: driver code

--- main.py
+++ main.py
@@ -1,58 +1,67 @@
 import re
 import os
+from typing import Tuple, TextIO
 from runner import RiscVRunner
 
-mc_label_regex = re.compile(r"^([0-9a-fA-F]{8}) <([a-zA-Z_]+)>:$")
 mc_instruction_regex = re.compile(r"^([0-9a-fA-F]{8}):\t([0-9a-fA-F]+)$")
+
+
+def build_code_bytearray(f: TextIO) -> bytearray:
+    code_bytes = bytearray()
+
+    supposed_next_addr = 0
+    for line in f:
+        try:
+            addr, instr = parse_mc_line(line)
+
+            # In case of gaps between addresses, add empty bytes to maintain alignment
+            if (skipped_bytes_count := addr - supposed_next_addr) > 0:
+                code_bytes.extend(b"\x00" * skipped_bytes_count)
+
+            code_bytes.extend(instr)
+            supposed_next_addr = addr + len(instr)
+        except ValueError:
+            pass
+
+    return code_bytes
 
 
 def run_mc_file(file_path: str):
     with open(file_path) as f:
         # Skip first line
         f.readline()
+        program_bytes = build_code_bytearray(f)
 
-        program_bytes = bytearray()
-        supposed_next_addr = 0
-        for line in f:
-            parsed_tuple = parse_mc_line(line)
-            if parsed_tuple is not None:
-                addr, instr = parsed_tuple
+    # Run the program
+    filename = os.path.basename(file_path)
+    print("========", (filename + " ").ljust(24, "="))
 
-                # Add empty bytes to maintain address alignment
-                if skipped_bytes_count := addr - supposed_next_addr:
-                    program_bytes.extend(b"\x00" * skipped_bytes_count)
-
-                program_bytes.extend(instr)
-
-                supposed_next_addr = addr + len(instr)
-
-        # Run the program
-        filename = os.path.basename(file_path)
-        print("========", (filename + " ").ljust(24, "="))
-
-        runner = RiscVRunner(program_bytes)
+    runner = RiscVRunner(program_bytes)
+    try: 
         runner.run()
+    except NotImplementedError as e:
+        print(e)
 
-        a0 = runner.registers[10]
-        if a0 == 1:
-            print("PASS: a0 = 1")
-        else:
-            print(f"FAIL: a0 = {a0}")
+    # Program finished. Inspect the return register, a0.
+    a0 = runner.registers[10]
+    if a0 == 1:
+        print("PASS: a0 = 1")
+    else:
+        # For some reason every test lshifts the failed test number by 1
+        print(f"FAIL: a0 = {a0 >> 1}")
+    print()
 
 
-def parse_mc_line(line: str):
-    if m := mc_label_regex.match(line):
-        # This is not actually used
-        address, label = m.groups()
-    elif m := mc_instruction_regex.match(line):
-        hex_address, hex_instruction = m.groups()
+def parse_mc_line(line: str) -> Tuple[int, bytes]:
+    if m := mc_instruction_regex.match(line):
+        hex_addr_str, hex_instr_str = m.groups()
 
-        instr_bytes = len(hex_instruction) // 2  # One byte is two hex digits
-
-        instr = bytearray.fromhex(hex_instruction)
-        addr = int(hex_address, 16) - 0x80000000
+        addr = int(hex_addr_str, 16) - 0x80000000
+        instr = bytes.fromhex(hex_instr_str)
 
         return addr, instr
+
+    raise ValueError(f"Line {line} does not match expected format.")
 
 
 def main():
